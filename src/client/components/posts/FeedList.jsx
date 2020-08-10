171c38230b576b69c3a774aaa7db703eba347bfc
luckyleonard <dzhu31@hotmail.com>
2020-08-10 19:31:21 +1000

    🎨 capsulate FeedList component

--- src/client/components/posts/FeedList.jsx
+++ src/client/components/posts/FeedList.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 
-function FeedList({ hasMore, fetchMore, loadMore, posts }) {
+function FeedList({ fetchMore, posts = [] }) {
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(0);
+
+  const loadMore = (fetchMore) => {
+    fetchMore({
+      variables: {
+        page: page + 1,
+      },
+      updateQuery(previousResult, { fetchMoreResult }) {
+        if (!fetchMoreResult.postsFeed.posts.length) {
+          setHasMore(false);
+          return previousResult;
+        }
+        setPage((page) => page + 1);
+        const newData = {
+          postsFeed: {
+            __typename: 'PostFeed',
+            posts: [
+              ...previousResult.postsFeed.posts,
+              ...fetchMoreResult.postsFeed.posts,
+            ],
+          },
+        };
+        return newData;
+      },
+    });
+  };
+
   return (
     <div className='feed'>
       <InfiniteScroll
